e624e265fa17062bbb9eccf85a7ee3730c31b0ad
Harini Shree R <harinishree085@gmail.com>
2025-08-07 10:56:26 +0530

    Create maxSubArray.java

---
+++ maxSubArray.java
@@ -0,0 +1,20 @@
+public class maxSubArray {
+    public static int subarray (int[] nums){
+        int maxi = Integer.MIN_VALUE;
+        int sum =0;
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            if(sum>maxi){
+                maxi = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+    public static void main (String[]args){
+        int [] nums ={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(subarray(nums));
+    }
+}
