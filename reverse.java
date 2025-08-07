20e030cbebdb10b53333eeb8a50aa299261e1bf2
Harini Shree R <harinishree085@gmail.com>
2025-08-07 10:57:56 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,7 @@
+public class reverse{
+    public static void main(String[]args){
+        String str = "Harini";
+        String rev = new StringBuilder(str).reverse().toString();
+        System.out.println(rev);
+    }
+}
